666276e45aa0dd75cc65ca377a07a484663a7df6
Prashant-cpu14 <prashantpratap32@gmail.com>
2025-09-12 15:59:38 +0530

    Update Average.java

--- Average.java
+++ Average.java
@@ -3,11 +3,12 @@ public class Average {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
+        int a = sc.nextInt(); // input by user 
+        int b = sc.nextInt(); // input by user 
+        int c = sc.nextInt(); // input by user 
         int avg = (a+b+c) /3;
         System.out.println("average :" +avg);
 
     }
+
 }
