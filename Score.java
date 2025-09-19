8ab5d7b23bcc8f8cb1abf99de45f7ae6f7166a44
Prashant Pratap Singh <prashantpratap32@gmail.com>
2025-09-19 06:49:23 +0530

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,8 @@
+public class Score {
+    public static void main(String[] args) {
+        int marks = 32;
+        String result = (marks>33)?"PASS":"FAIL";
+        System.out.println(result);
+
+    }
+}
