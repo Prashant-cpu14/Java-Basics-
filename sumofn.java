a845314691179b4e69672378c9abe70fd43c39ee
Prashant Pratap Singh <prashantpratap32@gmail.com>
2025-11-16 17:14:50 +0530

    Add files via upload

---
+++ sumofn.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sumofn {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while (i<=num) {
+               sum+=i;
+            i++;
+            }
+        System.out.println("your sum is " +sum);
+    }
+}
