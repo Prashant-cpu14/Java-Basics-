c635a1c75f0689e40abe869b88ddb85966a580fe
Prashant Pratap Singh <prashantpratap32@gmail.com>
2025-11-22 21:07:00 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,11 @@
+public class multiply {
+public static void main(String[] args) {
+    int n = 4;
+    for(int i=1; i<=10; i++){
+        int m = n*i;
+        System.out.println(n + " x " + i + " = " +m );
+
+    }
+}
+    
+}
