c247c44e373efbf8cc96a2f63cfadffcc85868c8
Prashant Pratap Singh <prashantpratap32@gmail.com>
2025-11-24 21:13:59 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,11 @@
+public class factorial {
+    public static void main(String[] args) {
+        int n=4;
+        long factorial=1;
+        for(int i=1;i<=n; i++){
+            factorial *=i;
+            }
+            System.out.println("Factorial of a number " +factorial);
+        
+    }
+}
